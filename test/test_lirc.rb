af39b88df6d551a09dfc523f57decb341c5d0a58
hans <hans@fugal.net>
2006-06-24 17:52:50 -0600

    a couple test
    darcs-hash:20060624235250-28bda-abfc30e92c379496c0d7d66ca9571bbdeeb7b4d9.gz

--- test/test_lirc.rb
+++ test/test_lirc.rb
@@ -8,12 +8,14 @@ class TC_LIRC < Test::Unit::TestCase
   def teardown
   end
   def test_event
-    e = Event.new("00000000000a8b59 00 power Sony-838")
+    ev1 = "00000000000a8b59 00 power Sony-838"
+    e = Event.new(ev1)
     assert_equal 0xa8b59, e.code
     assert_equal 0, e.repeat
     assert ! e.repeat?
     assert_equal "power", e.name
     assert_equal "Sony-838", e.remote
+    assert_equal ev1, e.to_s
 
     e = Event.new("00000000000a8b59 01 power Sony-838")
     assert_equal 0xa8b59, e.code
@@ -21,5 +23,14 @@ class TC_LIRC < Test::Unit::TestCase
     assert e.repeat?
     assert_equal "power", e.name
     assert_equal "Sony-838", e.remote
+
+    e = Event.new("00000000000a8b59 10 power Sony-838")
+    assert_equal 0x10, e.repeat
+    assert e.repeat?
+  end
+  def test_client
+    c = Client.new
+    puts "press a key on the remote now"
+    puts c.next
   end
 end
