42ac14e09d69ca96dfc22f1aa0e8c99cb0abf4d6
Denis Knauf <Denis.Knauf@gmail.com>
2010-04-04 02:07:59 +0200

    #each added, Rakefile modified for gems, README -> README.md

--- lib/lirc.rb
+++ lib/lirc.rb
@@ -1,26 +1,38 @@
 require 'socket'
 
 module LIRC
-  class Client
-    def initialize(dev="/dev/lircd")
-      @sock = UNIXSocket.open(dev)
-    end
-    def next
-      Event.new(@sock.readline)
-    end
-  end
-  class Event
-    attr_reader :code, :repeat, :name, :remote
-    def initialize(str)
-      code, repeat, @name, @remote = str.split(' ', 4)
-      @code = code.hex
-      @repeat = repeat.hex
-    end
-    def repeat?
-      @repeat > 0
-    end
-    def to_s
-      sprintf("%016x %02x %s %s", @code, @repeat, @name, @remote)
-    end
-  end
+	class Client
+		def initializer dev = nil
+			dev ||= "/dev/lircd"
+			@sock = UNIXSocket.open dev
+		end
+
+		def next
+			Event.new @sock.readline
+		end
+
+		def each_event
+			return Enumerator.new( self, :each_event)  unless block_given?
+			loop { yield self.next }
+		end
+		alias each each_event
+	end
+
+	class Event
+		attr_reader :code, :repeat, :name, :remote
+
+		def initialize str
+			code, repeat, @name, @remote = str.split(' ', 4)
+			@code = code.hex
+			@repeat = repeat.hex
+		end
+
+		def repeat?
+			@repeat > 0
+		end
+
+		def to_s
+			"%016x %02x %s %s" % [@code, @repeat, @name, @remote]
+		end
+	end
 end
